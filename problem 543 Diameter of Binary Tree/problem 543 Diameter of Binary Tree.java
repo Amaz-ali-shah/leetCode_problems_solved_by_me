29fb51fa751b185fb92744b3c96a13d069a98eb3
Amaz-ali-shah <syedamazalishah140@gmail.com>
2026-07-18 20:07:18 +0500

    Implement diameter of binary tree solution

---
+++ problem 543 Diameter of Binary Tree/problem 543 Diameter of Binary Tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        // Update the diameter if the path through current node is longer
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        // Return the height of the current subtree
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
