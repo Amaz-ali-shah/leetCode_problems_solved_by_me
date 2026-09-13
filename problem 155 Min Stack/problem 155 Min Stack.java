bd840387b2bc7ce0372f41795d50b462c12e64c9
Amaz-ali-shah <syedamazalishah140@gmail.com>
2026-09-13 23:40:09 +0500

    Implement MinStack with push, pop, top, and getMin

---
+++ problem 155 Min Stack/problem 155 Min Stack.java
@@ -0,0 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class MinStack {
+
+    private final Deque<Integer> stack;
+    private final Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
